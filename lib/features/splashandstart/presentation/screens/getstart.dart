8eb0bd4910a6e45b09bbfd966cb98f342d58f754
haazemsaid <haazemsaidd@gmail.com>
2025-01-28 01:27:38 +0200

    Add tablet layout for Get Started screen and update layout logic

--- lib/features/splashandstart/presentation/screens/getstart.dart
+++ lib/features/splashandstart/presentation/screens/getstart.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../layouts/desktop/getStartedDesktopLayout.dart';
 import '../layouts/mobile/mobilegetstartlayout.dart';
+import '../layouts/tablet/getstarttable.dart';
 
 class Getstart extends StatelessWidget {
   const Getstart({super.key});
@@ -10,10 +11,12 @@ class Getstart extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(body: LayoutBuilder(builder: (context, constrains) {
       print(constrains.maxWidth);
-      if (constrains.maxWidth >= 800) {
-        return getStartedDesktopLayout();
-      } else {
+      if (constrains.maxWidth < 600) {
         return getstartmobileViewLayout();
+      } else if (constrains.maxWidth < 1200) {
+        return getStartTabletlayout();
+      } else {
+        return getStartedDesktopLayout();
       }
     }));
   }

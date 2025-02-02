3217552104fa1ada42d0a3480b7338cff1715ec5
haazemsaid <haazemsaidd@gmail.com>
2025-02-03 01:27:41 +0200

    Refactor desktop and mobile layouts for improved responsiveness and structure

--- lib/features/splashandstart/presentation/layouts/desktop/getStartedDesktopLayout.dart
+++ lib/features/splashandstart/presentation/layouts/desktop/getStartedDesktopLayout.dart
@@ -12,40 +12,53 @@ class getStartedDesktopLayout extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        SizedBox(width: double.infinity, height: 120),
-        Padding(
-          padding: const EdgeInsets.only(left: 12.0, top: 20),
-          child: splashImage(),
+        SizedBox(
+            width: double.infinity,
+            height: MediaQuery.sizeOf(context).height * 0.08),
+        Expanded(
+          flex: 9,
+          child: Padding(
+            padding: const EdgeInsets.only(left: 12.0, top: 20),
+            child: splashImage(),
+          ),
         ),
         SizedBox(width: double.infinity, height: 55),
         Text(
           'اقرأ القرآن الكريم بطريقة صحيحة',
           style: Appconstrains.tajawal_bold.copyWith(color: Colors.black38),
         ),
-        SizedBox(width: double.infinity, height: 70),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            getStartedTextWidget(
-              title: 'تسجيل دخول',
-              colors: [
-                Appconstrains.primaryColor,
-                Appconstrains.primaryColor,
-                const Color.fromARGB(255, 14, 223, 195),
-              ],
-              textColor: Appconstrains.secondaryColor,
-            ),
-            SizedBox(width: 20),
-            getStartedTextWidget(
-              title: 'انشاء حساب',
-              colors: [
-                Appconstrains.secondaryColor,
-                Appconstrains.secondaryColor,
-              ],
-              textColor: Appconstrains.primaryColor,
-            ),
-          ],
-        )
+        SizedBox(
+            width: double.infinity,
+            height: MediaQuery.sizeOf(context).height * 0.087),
+        Expanded(
+          flex: 2,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              getStartedTextWidget(
+                title: 'تسجيل دخول',
+                colors: [
+                  Appconstrains.primaryColor,
+                  Appconstrains.primaryColor,
+                  const Color.fromARGB(255, 14, 223, 195),
+                ],
+                textColor: Appconstrains.secondaryColor,
+              ),
+              SizedBox(width: 20),
+              getStartedTextWidget(
+                title: 'انشاء حساب',
+                colors: [
+                  Appconstrains.secondaryColor,
+                  Appconstrains.secondaryColor,
+                ],
+                textColor: Appconstrains.primaryColor,
+              ),
+            ],
+          ),
+        ),
+        SizedBox(
+            width: double.infinity,
+            height: MediaQuery.sizeOf(context).height * 0.09),
       ],
     );
   }
--- lib/features/splashandstart/presentation/layouts/mobile/mobilegetstartlayout.dart
+++ lib/features/splashandstart/presentation/layouts/mobile/mobilegetstartlayout.dart
@@ -13,74 +13,74 @@ class getstartmobileViewLayout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        SizedBox(
-            width: double.infinity,
-            height: MediaQuery.sizeOf(context).height * 0.08),
-        Expanded(
-          flex: 9,
-          child: Padding(
-            padding: const EdgeInsets.only(left: 12.0, top: 20),
-            child: splashImage(),
-          ),
-        ),
-        SizedBox(
-            width: double.infinity,
-            height: MediaQuery.sizeOf(context).height * 0.1),
-        Expanded(
-          flex: 1,
-          child: Text(
-            'اقرأ القرآن الكريم بطريقة صحيحة',
-            style: Appconstrains.tajawal_bold.copyWith(color: Colors.black38),
-          ),
-        ),
-        SizedBox(
-            width: double.infinity,
-            height: MediaQuery.sizeOf(context).height * 0.06),
-        Expanded(
-          flex: 1,
-          child: GestureDetector(
-            onTap: () {
-              //in future check if user is loged in before
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+                width: double.infinity, height: constraints.maxHeight * 0.08),
+            Expanded(
+              flex: 9,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 12.0, top: 20),
+                child: splashImage(),
+              ),
+            ),
+            SizedBox(
+                width: double.infinity, height: constraints.maxHeight * 0.1),
+            Expanded(
+              flex: 1,
+              child: Text(
+                'اقرأ القرآن الكريم بطريقة صحيحة',
+                style:
+                    Appconstrains.tajawal_bold.copyWith(color: Colors.black38),
+              ),
+            ),
+            SizedBox(
+                width: double.infinity, height: constraints.maxHeight * 0.06),
+            Expanded(
+              flex: 1,
+              child: GestureDetector(
+                onTap: () {
+                  //in future check if user is logged in before
 
-              context.go(MyRoute.onbording);
-            },
-            child: getStartedTextWidget(
-              title: 'تسجيل دخول',
-              colors: [
-                Appconstrains.primaryColor,
-                Appconstrains.primaryColor,
-                const Color.fromARGB(255, 14, 223, 195),
-              ],
-              textColor: Appconstrains.secondaryColor,
+                  context.go(MyRoute.onbording);
+                },
+                child: getStartedTextWidget(
+                  title: 'تسجيل دخول',
+                  colors: [
+                    Appconstrains.primaryColor,
+                    Appconstrains.primaryColor,
+                    const Color.fromARGB(255, 14, 223, 195),
+                  ],
+                  textColor: Appconstrains.secondaryColor,
+                ),
+              ),
             ),
-          ),
-        ),
-        SizedBox(
-            width: double.infinity,
-            height: MediaQuery.sizeOf(context).height * 0.06),
-        Expanded(
-          flex: 1,
-          child: GestureDetector(
-            onTap: () {
-              context.go(MyRoute.onbording);
-            },
-            child: getStartedTextWidget(
-              title: 'انشاء حساب',
-              colors: [
-                Appconstrains.secondaryColor,
-                Appconstrains.secondaryColor,
-              ],
-              textColor: Appconstrains.primaryColor,
+            SizedBox(
+                width: double.infinity, height: constraints.maxHeight * 0.06),
+            Expanded(
+              flex: 1,
+              child: GestureDetector(
+                onTap: () {
+                  context.go(MyRoute.onbording);
+                },
+                child: getStartedTextWidget(
+                  title: 'انشاء حساب',
+                  colors: [
+                    Appconstrains.secondaryColor,
+                    Appconstrains.secondaryColor,
+                  ],
+                  textColor: Appconstrains.primaryColor,
+                ),
+              ),
             ),
-          ),
-        ),
-        SizedBox(
-            width: double.infinity,
-            height: MediaQuery.sizeOf(context).height * 0.06),
-      ],
+            SizedBox(
+                width: double.infinity, height: constraints.maxHeight * 0.06),
+          ],
+        );
+      },
     );
   }
 }
